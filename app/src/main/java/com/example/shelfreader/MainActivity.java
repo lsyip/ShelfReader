1290bfe37818b94c470a759b4caf0d77692ef2ef
lsyip <lsyip62@gmail.com>
2021-06-03 23:02:50 -0400

    camera preview setup

--- app/src/main/java/com/example/shelfreader/MainActivity.java
+++ app/src/main/java/com/example/shelfreader/MainActivity.java
@@ -1,23 +1,56 @@
 package com.example.shelfreader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button readButton = findViewById(R.id.read_button);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasCameraPermission()) {
+                    enableCamera();
+                } else {
+                    requestPermission();
+                }
+            }
+        });
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
     }
 
     // Launch the shelf reader activity
-    public void launchReader(View v) {
-        Intent i = new Intent(this, ShelfReader.class);
-        startActivity(i);
+    private void enableCamera() {
+        Intent intent = new Intent(this, ShelfReader.class);
+        startActivity(intent);
     }
 
     // Launch the log history activity
